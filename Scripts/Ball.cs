7ac14890b9b4993fdeeaa31c208ea9a74d2bb34f
Khaireddine Benjemaa <62791525+TRSescada@users.noreply.github.com>
2022-12-02 13:22:04 +0100

    Add files via upload

---
+++ Scripts/Ball.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public Animator BallAnim;
+    public Rigidbody BallRB;
+    public CameraFollow Camera;
+    public GameObject SplashFX;
+    public GameObject GameOverPanel;
+    public AudioSource Audio;
+    public AudioClip BoltAudio;
+    public AudioClip CoinAudio;
+
+    public float ForceValue;
+
+    [HideInInspector]
+    public bool IsFly;
+    public static bool IsGameOver;
+
+    private bool _setUpvelocity;
+
+    private void Start()
+    {
+        IsFly = false;
+        IsGameOver = false;
+        Physics.gravity = new Vector3(0, -20f, 0);
+    }
+
+    private void LateUpdate()
+    {
+        if (_setUpvelocity)
+        {
+            Audio.PlayOneShot(BoltAudio);
+            BallAnim.SetTrigger("Jump");
+            BallRB.velocity = new Vector3(0f, 8f, 0f);
+            _setUpvelocity = false;
+            IsFly = false;
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Fail")
+        {
+            Fail();   
+        }
+        else
+        {
+            _setUpvelocity = true;
+            GameObject particle = Instantiate(SplashFX, other.transform, false);
+            particle.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 0.01f, other.transform.position.z - 1.5f);
+            particle.transform.rotation = Quaternion.Euler(90f, 0, Random.Range(0, 360f));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "HelixPlatform")
+        {
+            ScoreManager.Instance.AddScore();
+            Audio.PlayOneShot(CoinAudio);
+            IsFly = true;
+        }
+    }
+
+    private void Fail()
+    {
+        IsGameOver = true;
+        ScoreManager.Instance.SetBestScore();
+        GameOverPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Restert()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Scripts/UpLimit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpLimit : MonoBehaviour {
+
+    public Transform StartTransform;
+
+	private void Update () {
+        RaycastHit hit;
+
+        Debug.DrawRay(StartTransform.position, -transform.forward * 5f, Color.red);
+
+        if (!Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z + 2f), -transform.forward * 5f, out hit))
+        {
+            GetComponent<HelixPieceCreator>().ResetNewPiece();
+            print("Change pos");
+        }
+	}
+}
