f3ade7ded460461337bdb1db89921d3f8623a613
Khaireddine Benjemaa <62791525+TRSescada@users.noreply.github.com>
2022-12-02 13:22:54 +0100

    Jumer Scripts

---
+++ Scripts/MoveController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static Action Move;
+
+    public static Vector2 delta;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        delta = eventData.position;
+        Move();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        delta = eventData.position;
+        Move();
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        delta = eventData.position;
+    }
+}
